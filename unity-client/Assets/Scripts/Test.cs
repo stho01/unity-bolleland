0e86b17462bec6a294a4ce8fadb9bec9cc54ffe7
Sten Marius Holmen <sten.marius.holmen@gmail.com>
2023-01-27 12:43:43 +0100

    Add test script

---
+++ unity-client/Assets/Scripts/Test.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public Animator animator;
+    public AnimatorController jumpAnimation;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+           
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            
+        }
+    }
+}
